0db370a02befc29dd36adbce908d5ecb98397a43
kathiravan B <itsmekathi92@gmail.com>
2020-08-01 21:23:01 +0530

    Basic Color Changer extension

--- angular-chrome-extension/src/app/app.component.ts
+++ angular-chrome-extension/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-chrome-extension';
+  color: string;
+  constructor() {}
+  ngOnInit(): void {
+    chrome.storage.sync.get('color', ({ color }) => {
+      this.color = color;
+    });
+  }
+
+  public colorize(): void {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: 'document.body.style.backgroundColor = "' + this.color + '";',
+      });
+    });
+  }
+  public updateColor(color: string): void {
+    chrome.storage.sync.set({ color });
+  }
 }
--- angular-chrome-extension/src/background.ts
+++ angular-chrome-extension/src/background.ts
@@ -1,7 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' });
   chrome.webNavigation.onCompleted.addListener(
     () => {
       chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        console.log('the id is: ', id);
         chrome.pageAction.show(id);
       });
     },
